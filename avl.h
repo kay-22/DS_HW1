732dff92fef7ac0553f6b17ab8eb14c6aa4ed335
Offek Hemo <napoel92@campus.technion.ac.il>
2020-11-22 20:57:00 +0200

    basic classes

---
+++ avl.h
@@ -0,0 +1,24 @@
+#include <assert.h>
+
+/* 
+    Node* left\right that is nulptr means subtree with hight=(-1)
+ */
+template <typename T>
+class Node{
+    T data;
+    Node* left;
+    Node* right;
+
+    int subTreeHight;
+
+};
+
+
+
+/* 
+    Node* root that is nulptr means an empty tree
+ */
+template  <typename T>
+class AvlTree{
+    Node* root;
+};
