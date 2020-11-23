8760c7f31b1144ca513846a1d644bc503c50c1a1
Offek Hemo <napoel92@campus.technion.ac.il>
2020-11-23 22:02:37 +0200

    parent ptr added

--- avl.h
+++ avl.h
@@ -3,13 +3,15 @@
 
 
 
-///////////////////////////////////////////  do-cosmetics ////////////////////////////////////////////////////////
+
 /* 
-    Node* left\right that is nulptr means subtree with hight=(-1)
+    * general description: an auxiliary struct to represent a vertex in an avl tree.
+    left\right that is nullptr means subtree with hight=(-1)
+
  */
 template <typename Key, typename Data>
-class Node {
-    Key key;
+struct Node {
+    const Key key;
     Data data;
     Node* left;
     Node* right;
@@ -19,40 +21,14 @@ class Node {
     // assuming Data and Key has copy ctor
     explicit Node(Key key, Data data, Node* left = nullptr, Node* right= nullptr, int subTreeHight = 0) 
         : key(key), data(data), left(left), right(right), subTreeHight(subTreeHight) {}    
-
-    Data getData() {
-        //assuming Data has copy ctor
-        return data;
-    }
-
-    void setData(const Data& other_data) {
-        //assuming Data has operator=()
-        data = other_data;
-    }
-
-    Key getKey() {
-        //assuming Key has copy ctor
-        return key;
-    }
-
-    int getHight() {
-        return subTreeHight;
-    }
-
-    void setHight(int hight) {
-        subTreeHight = hight;
-    }
-
+        
 };
-///////////////////////////////////////////  do-cosmetics ////////////////////////////////////////////////////////
-
-
-
 
 
 
 /* 
-    Node* root that is nulptr means an empty tree
+    root that is nullptr means an empty tree
+
  */
 template  <typename Key,typename Data>
 class AvlTree{
@@ -65,53 +41,67 @@ class AvlTree{
     
     // returns the required node or null if failed. last <- last checked node
     Node<Key,Data>* findNode(const Key& key, Node<Key,Data>* last);
-    public:
 
+    public:
     Data* find(const Key& key);
     bool insert(const Key& key, const Data& data);
     bool remove(const Key& key);
 
 
-    //exceptions:
 
 };
 
 
+
+
 //assuming: Key has operators: < , > , =
+//last remains nullptr in case what we are looking for is the root itself
 template<typename Key,typename Data>
 Node<Key,Data>* AvlTree<Key,Data>::findNode(const Key& key, Node<Key,Data>* last){
-        if( !last ){
-            throw NullArgument();
+    last = nullptr;
+    Node<Key,Data>* current = root;
+
+    while( current!=nullptr  &&  key!=current->key() ){
+        last=current;
+
+        if( key < current->key() ){
+            current=current->left;
+        }else if( key > current->key() ){
+            current=current->right;
         }
-
-        last = root;
-        Node<Key,Data>* current = root;
-        
-        while( current!=nullptr  &&  key!=current->key ){
-            last=current;
-
-            if( key < current->key ){
-                current=current->left;
-            }else if( key > current->key ){
-                current=current->right;
-            }
-        }
-
-        return current;
+    }
+    return current;
 }
+
+
+
+//returns null in case of failure
+template<typename Key,typename Data>
+Data* AvlTree<Key,Data>::find(const Key& key){
+    Node<Key,Data>* lastOnSearch;
+    Node<Key,Data>* desired = findNode(key,lastOnSearch);
+
+    //if(desired!=nullptr){
+    //    return &(desired->data);
+    //}
+    //return nullptr;
+}
+
+
 
 template<typename Key,typename Data>
-Data* find(const Key& key){
-    return &Data();
+bool AvlTree<Key,Data>::insert(const Key& key, const Data& data){
+    Node<Key,Data>* lastOnSearch;
+    Node<Key,Data>* exists = findNode(key,lastOnSearch);
+    if( exists ) return false;
+
+
+
+    
 }
+
 
 template<typename Key,typename Data>
-bool insert(const Key& key, const Data& data){
-    return false;
-}
-
-
-template<typename Key>
- bool remove(const Key& key){
+ bool AvlTree<Key,Data>::remove(const Key& key){
      return true;
  }
