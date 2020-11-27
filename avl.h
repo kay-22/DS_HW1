c98ca8873a54857af686343039c2884ba73bbd7c
Offek Hemo <napoel92@campus.technion.ac.il>
2020-11-27 19:05:34 +0200

    switched to shared_ptr to avoid manual_mem_management

--- avl.h
+++ avl.h
@@ -1,6 +1,6 @@
 #include <assert.h>
+#include <memory>
 #include "Exceptions.h"
-
 
 
 
@@ -11,22 +11,23 @@
  */
 template <typename Key, typename Data>
 struct Node {
+    typedef std::shared_ptr<Node<Key,Data>> NodePtr;
+
     const Key key;
     Data data;
-    Node* parent;
-    Node* left;
-    Node* right;
+    NodePtr parent;
+    NodePtr left;
+    NodePtr right;
 
     
     int subTreeHight;
 
     // assuming Data and Key has copy ctor
-    explicit Node(Key key, Data data, Node* parent=nullptr, Node* left = nullptr, Node* right= nullptr,
-                  int subTreeHight = 0) 
-                : key(key), data(data), left(left), right(right), parent(parent) ,subTreeHight(subTreeHight) {}    
+    explicit Node(Key key, Data data,  NodePtr parent=nullptr,  NodePtr left = nullptr,
+        NodePtr right= nullptr, int subTreeHight = 0) : key(key), data(data),
+        left(left), right(right), parent(parent) ,subTreeHight(subTreeHight) {}    
         
 };
-
 
 
 
@@ -38,66 +39,66 @@ template  <typename Key,typename Data>
 class AvlTree{
 
     public:
-    explicit AvlTree(Node<Key,Data>* root = nullptr);
-    Data* find(const Key& key);
-    bool insert(const Key& key, const Data& data);
-    bool remove(const Key& key);
+    explicit AvlTree( std::shared_ptr<Node<Key,Data>> root = nullptr);
+    std::shared_ptr<Data>  find(const Key& key);
+    bool     insert(const Key& key, const Data& data);
+    bool     remove(const Key& key);
     
     
     // Non-interface part:
     ~AvlTree(); //   <---TODO
     private:
-    Node<Key,Data>* root;
+    std::shared_ptr<Node<Key,Data>> root;
 
     //AvlTree(const AvlTree& other) = delete;
 
     /* returns the required node or null if failed. last <- last checked node */
-    Node<Key,Data>* findNode(const Key& key, Node<Key,Data>* last);
+    std::shared_ptr<Node<Key,Data>> findNode(const Key& key, std::shared_ptr<Node<Key,Data>> last);
     /* inserts to the AvlTree like BinaryTreeInsert. assuming "not-exists". returns a poiner to the added node. */
-    Node<Key,Data>* binaryInsert(const Key& key, const Data& data, Node<Key,Data>* last );
+    std::shared_ptr<Node<Key,Data>> binaryInsert(const Key& key, const Data& data, std::shared_ptr<Node<Key,Data>> last );
     /* removes from the AvlTree like BinaryTreeRemove. assuming "exists". returns a pointr to the lowest on track. */
-    Node<Key,Data>* binaryRemove(Node<Key,Data>* deleted);
+    std::shared_ptr<Node<Key,Data>> binaryRemove(std::shared_ptr<Node<Key,Data>> deleted);
     /* assures that the balanceFactor of the node is correct by applying "role-over" if neccessary. */
-    void assureBalance(Node<Key,Data>* nodeOnTrack);
+    void assureBalance(std::shared_ptr<Node<Key,Data>> nodeOnTrack);
     /* performs a simple rotation to the left where pivit is grandSon of the un-balanced vertex */
-    void rotateLeft(Node<Key,Data>* pivot);
+    void rotateLeft(std::shared_ptr<Node<Key,Data>> pivot);
 
 };
 
 //declerations:
 
     template  <typename Key,typename Data>
-    Node<Key,Data>* findSuccessor(Node<Key,Data>* deleted);
+    std::shared_ptr<Node<Key,Data>> findSuccessor(std::shared_ptr<Node<Key,Data>> deleted);
     
     template  <typename Key,typename Data>
-    void swapAvlNodes(Node<Key,Data>* v, Node<Key,Data>* u);
+    void swapAvlNodes(std::shared_ptr<Node<Key,Data>> v, std::shared_ptr<Node<Key,Data>> u);
 
     template  <typename Key,typename Data>
-    void swapNodePointers(Node<Key,Data>* ptr1, Node<Key,Data>* ptr2);
+    void swapNodePointers(std::shared_ptr<Node<Key,Data>> ptr1,std::shared_ptr<Node<Key,Data>> ptr2);
 
     template  <typename Key,typename Data>
-    void removeLeaf(Node<Key,Data>* deleted, bool deletedIsLeft);
+    void removeLeaf(std::shared_ptr<Node<Key,Data>> deleted, bool deletedIsLeft);
 
     template  <typename Key,typename Data>
-    void removeVertexWithOneSon(Node<Key,Data>* deleted, bool deletedIsLeft);
+    void removeVertexWithOneSon(std::shared_ptr<Node<Key,Data>> deleted, bool deletedIsLeft);
 
     template  <typename Key,typename Data>
-    void rotateRight(Node<Key,Data>* pivot);
+    void rotateRight(std::shared_ptr<Node<Key,Data>> pivot);
     
 
 //end declerations
 
 //by default: create an empty new AvlTree
 template  <typename Key,typename Data>
-AvlTree<Key,Data>::AvlTree(Node<Key,Data>* root) : root(root) {}
+AvlTree<Key,Data>::AvlTree(std::shared_ptr<Node<Key,Data>> root) : root(root) {}
 
 
 //assuming: Key,Data has operators: < , > , =
 //last remains nullptr in case what we are looking for is the root itself
 template<typename Key,typename Data>
-Node<Key,Data>* AvlTree<Key,Data>::findNode(const Key& key, Node<Key,Data>* last){
+std::shared_ptr<Node<Key,Data>> AvlTree<Key,Data>::findNode(const Key& key, std::shared_ptr<Node<Key,Data>> last){
     last = nullptr;
-    Node<Key,Data>* current = root;
+    std::shared_ptr<Node<Key,Data>> current = root;
 
     while( current!=nullptr  &&  key!=current->key() ){
         last=current;
@@ -113,10 +114,10 @@ Node<Key,Data>* AvlTree<Key,Data>::findNode(const Key& key, Node<Key,Data>* last
 
 //assuming that deleted has 2 sons
 template  <typename Key,typename Data>
-inline Node<Key,Data>* findSuccessor(Node<Key,Data>* predecessor){
+inline std::shared_ptr<Node<Key,Data>> findSuccessor(std::shared_ptr<Node<Key,Data>> predecessor){
     assert(predecessor->right && predecessor->left);
     
-    Node<Key,Data> currentNode = predecessor->right;
+    std::shared_ptr<Node<Key,Data>> currentNode = predecessor->right;
     while (currentNode->left) {
         currentNode = currentNode->left;
     }
@@ -126,8 +127,8 @@ inline Node<Key,Data>* findSuccessor(Node<Key,Data>* predecessor){
 }
 
 template  <typename Key,typename Data>
-inline void swapNodePointers(Node<Key,Data>* ptr1, Node<Key,Data>* ptr2){  
-    Node<Key,Data>* temp;
+inline void swapNodePointers(std::shared_ptr<Node<Key,Data>> ptr1, std::shared_ptr<Node<Key,Data>> ptr2){  
+    std::shared_ptr<Node<Key,Data>> temp;
 
     temp = ptr1;
     ptr1 = ptr2;
@@ -135,7 +136,7 @@ inline void swapNodePointers(Node<Key,Data>* ptr1, Node<Key,Data>* ptr2){
 }
 
 template  <typename Key,typename Data>
-inline void swapAvlNodes(Node<Key,Data>* v, Node<Key,Data>* u){
+inline void swapAvlNodes(std::shared_ptr<Node<Key,Data>> v, std::shared_ptr<Node<Key,Data>> u){
     swapNodePointers(v->parent,u->parent);
     swapNodePointers(v->right,u->right);
     swapNodePointers(v->left,u->left);
@@ -145,8 +146,9 @@ inline void swapAvlNodes(Node<Key,Data>* v, Node<Key,Data>* u){
 //assuming: Key,Data has copy Ctor and operator=
 //assuming: Key,Data has operators: < , > , =
 template  <typename Key,typename Data>
-Node<Key,Data>* AvlTree<Key,Data>::binaryInsert(const Key& key, const Data& data, Node<Key,Data>* last ){
-    Node<Key,Data>* newNode = new Node<Key,Data>(key,data,last);
+std::shared_ptr<Node<Key,Data>> AvlTree<Key,Data>::binaryInsert(const Key& key, const Data& data,
+                                                                std::shared_ptr<Node<Key,Data>> last ){
+    std::shared_ptr<Node<Key,Data>> newNode( new Node<Key,Data>(key,data,last) );
     
     if( last==nullptr ){//meaning an empty tree
         assert( this->root==nullptr );
@@ -167,7 +169,7 @@ Node<Key,Data>* AvlTree<Key,Data>::binaryInsert(const Key& key, const Data& data
 }
 
 template<typename Key, typename Data>
-inline void removeLeaf(Node<Key,Data>* deleted, bool deletedIsLeft){
+inline void removeLeaf(std::shared_ptr<Node<Key,Data>> deleted, bool deletedIsLeft){
 
     if (deletedIsLeft){
         deleted->parent->left = nullptr;
@@ -179,7 +181,7 @@ inline void removeLeaf(Node<Key,Data>* deleted, bool deletedIsLeft){
 
 
 template<typename Key, typename Data>
-inline void removeVertexWithOneSon(Node<Key,Data>* deleted, bool deletedIsLeft){
+inline void removeVertexWithOneSon(std::shared_ptr<Node<Key,Data>> deleted, bool deletedIsLeft){
     if(deletedIsLeft && deleted->left) {deleted->parent->left= deleted->left;}
     else if (deletedIsLeft && deleted->right) {deleted->parent->left= deleted->right;}
     else if (!deletedIsLeft && deleted->left) {deleted->parent->right= deleted->left;}
@@ -195,18 +197,18 @@ void rotateRight(Node<Key,Data>* pivot){
 }
 
 template<typename Key, typename Data>
-void AvlTree<Key,Data>::rotateLeft(Node<Key,Data>* pivot){
+void AvlTree<Key,Data>::rotateLeft(std::shared_ptr<Node<Key,Data>> pivot){
 
     assert( pivot->parent );
-    Node<Key,Data>* oldFather = pivot->parent->parent
+    std::shared_ptr<Node<Key,Data>> oldFather = pivot->parent->parent;
     assert( oldFather->right==pivot );
 
-    Node<Key,Data>* oldGrandFather = pivot->parent->parent;
+    std::shared_ptr<Node<Key,Data>> oldGrandFather = pivot->parent->parent;
     pivot->parent = oldGrandFather;
     if( oldGrandFather==nullptr ) { ////////////////////////////////parent is the root
         root = pivot;
     }else if( oldGrandFather->right==pivot->parent ){///////////////parent is a rightSon
-        oldGrandFather->right = pivot
+        oldGrandFather->right = pivot;
     }else{ assert( oldGrandFather->left==pivot->parent ); //////////parent is a leftSon
         oldGrandFather->left = pivot;
     }
@@ -221,7 +223,7 @@ void AvlTree<Key,Data>::rotateLeft(Node<Key,Data>* pivot){
 
 
 template<typename Key, typename Data>
-Node<Key,Data>* AvlTree<Key,Data>::binaryRemove(Node<Key,Data>* deleted){
+std::shared_ptr<Node<Key,Data>> AvlTree<Key,Data>::binaryRemove(std::shared_ptr<Node<Key,Data>> deleted){
     assert( deleted!=nullptr );
 
     bool deletedIsLeft = (deleted->parent->left == deleted);
@@ -236,7 +238,7 @@ Node<Key,Data>* AvlTree<Key,Data>::binaryRemove(Node<Key,Data>* deleted){
     }
     else { assert(deleted->left && deleted->right); 
     // meaning deleted has two sons
-        Node<Key, Data>* successor = findSuccessor(deleted);
+        std::shared_ptr<Node<Key,Data>> successor = findSuccessor(deleted);
         swapAvlNodes(deleted, successor);
 
         assert(deleted->left == nullptr);
@@ -244,7 +246,7 @@ Node<Key,Data>* AvlTree<Key,Data>::binaryRemove(Node<Key,Data>* deleted){
         else removeVertexWithOneSon(deleted);
     }
 
-    Node<Key,Data>* lastOnTrack = deleted->parent;
+    std::shared_ptr<Node<Key,Data>> lastOnTrack = deleted->parent;
     delete deleted;
 
     return lastOnTrack;
@@ -253,16 +255,16 @@ Node<Key,Data>* AvlTree<Key,Data>::binaryRemove(Node<Key,Data>* deleted){
 
 
 template  <typename Key,typename Data>
-void AvlTree<Key,Data>::assureBalance(Node<Key,Data>* nodeOnTrack){
+void AvlTree<Key,Data>::assureBalance(std::shared_ptr<Node<Key,Data>> nodeOnTrack){
     //todo
     return;
 }
 
 //returns null in case of failure
 template<typename Key,typename Data>
-Data* AvlTree<Key,Data>::find(const Key& key){
-    Node<Key,Data>* lastOnSearch;
-    Node<Key,Data>* desired = findNode(key,lastOnSearch);
+std::shared_ptr<Data> AvlTree<Key,Data>::find(const Key& key){
+    std::shared_ptr<Node<Key,Data>>lastOnSearch;
+    std::shared_ptr<Node<Key,Data>> desired = findNode(key,lastOnSearch);
 
     if(desired!=nullptr){
         return &(desired->data);
@@ -275,11 +277,11 @@ Data* AvlTree<Key,Data>::find(const Key& key){
 //return values: True for "sucssess", False for  "already exists".
 template<typename Key,typename Data>
 bool AvlTree<Key,Data>::insert(const Key& key, const Data& data){
-    Node<Key,Data>* lastOnSearch;
-    Node<Key,Data>* exists = findNode(key,lastOnSearch);
+    std::shared_ptr<Node<Key,Data>> lastOnSearch;
+    std::shared_ptr<Node<Key,Data>> exists = findNode(key,lastOnSearch);
     
     if( exists ) return false;
-    Node<Key,Data>* nodeOnTrack = binaryInsert(key,data,lastOnSearch);
+    std::shared_ptr<Node<Key,Data>> nodeOnTrack = binaryInsert(key,data,lastOnSearch);
     
     do{ assureBalance(nodeOnTrack);//  <--should also check suc
         nodeOnTrack = nodeOnTrack->parent;
@@ -295,11 +297,11 @@ bool AvlTree<Key,Data>::insert(const Key& key, const Data& data){
 //return values: True for "sucssess", False for  "not exists".
 template<typename Key,typename Data>
  bool AvlTree<Key,Data>::remove(const Key& key){
-    Node<Key,Data>* lastOnSearch;
-    Node<Key,Data>* exists = findNode(key,lastOnSearch);
+    std::shared_ptr<Node<Key,Data>> lastOnSearch;
+    std::shared_ptr<Node<Key,Data>> exists = findNode(key,lastOnSearch);
 
     if( !exists ) return false;
-    Node<Key,Data>* nodeOnTrack = binaryRemove(exists);
+    std::shared_ptr<Node<Key,Data>> nodeOnTrack = binaryRemove(exists);
 
     do{ assureBalance(nodeOnTrack);//  <--should also check succsess
         nodeOnTrack = nodeOnTrack->parent;
