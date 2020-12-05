781e359538737444a19b6f2d54497ffad7f601ce
kay-22 <try1.zipzap@gmail.com>
2020-12-05 19:36:50 +0200

    added Auxiliaries: Lecture, Time, and Course structs

---
+++ Auxiliaries.h
@@ -0,0 +1,31 @@
+#ifndef AUXILIARIES_H
+#define AUXILIARIES_H
+
+#include "List.h"
+#include "AvlTree.h"
+#include <memory>
+
+// linked list, such that every element in it is a linked lists of lectures with time_views = this->time
+struct Time {
+    int time;
+    list::List<Lecture> lectures;
+};
+
+//Lecture is a list element that holds an iterator of the current time_views
+struct Lecture {
+    list::List<Time>::const_iterator currentTime;
+    int courseID;
+
+    Lecture(const list::List<Time>& timeAxis, int courseID) 
+        : currentTime(timeAxis.begin()), courseID(courseID) {}
+};
+
+struct Course {
+    avlTree::AvlTree<int,Lecture*> lectures;
+    list::List<Time>::iterator maxTime;
+};
+
+
+#endif    //AUXILIARIES_H
+
+
