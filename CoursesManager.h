8c3a5ee0e602f179c0e4fb417c581455a80b4ffa
napoel92 <napoel92@campus.technion.ac.il>
2020-12-21 17:27:13 +0200

    before debuging of QUIT

--- CoursesManager.h
+++ CoursesManager.h
@@ -8,24 +8,41 @@
 
 
 
+/**
+* CoursesManager - a data type to manage online-courses system.
+* each course has its lectures, and each lecture has its time of view.
+* in addition, all times of view are layed on a sorted time-axis - and acssesible from it.
+*------------------------------------------------------------------------------------------
+* courses  - an avl-tree of all the Courses in the system
+* timeAxis - a two-way linked-list of all Times that represented on the systems time axis
+*/
 class CoursesManager {
     avlTree::AvlTree<int,Course> courses;
     list::List<Time> timeAxis;
 
 public:
-    CoursesManager();
 
-    //return values: True for "sucssess", False for  "already exists".
+    // return values: True for "sucssess", False for  "already exists".
+    // adds the course with all its lectures to the system with no watches at all
     bool AddCourse (int courseID, int numOfClasses);
-    //return values: True for "sucssess", False for  "doesn't exists".
+
+    // return values: True for "sucssess", False for  "doesn't exists".
+    // removes the course with all its lectures (with all their watches) from the system 
     bool RemoveCourse(int courseID);
 
+    // returns the status of the action.
+    // adds "time" to the time that the specific lecture has already got
     StatusType WatchClass(int courseID, int classID, int time);
 
+    // returns the status of the action.
+    // puts the time of view that related to courseId->classID inside *timeViewed
     StatusType TimeViewed(int courseID, int classID, int *timeViewed);
 
+    // returns the status of the action.
+    // puts the most viewed lectures in *classes and accordingly their courses in *courses
     StatusType GetMostViewedClasses(int numOfClasses, int *courses, int *classes);
 
+    CoursesManager();
     ~CoursesManager()=default;
 };
 
